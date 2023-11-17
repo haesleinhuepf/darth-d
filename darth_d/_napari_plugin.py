29bb35ca8c06855ecef6d5f4b5c4194a3737e8c3
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-11-17 20:08:53 +0100

    enable replace using dall-e-3

--- darth_d/_napari_plugin.py
+++ darth_d/_napari_plugin.py
@@ -76,9 +76,9 @@ def vary_gui_dalle3(input_image:"napari.types.ImageData", image_size:str="1024x1
 @register_function(menu="Generate > Replace masked region (DALL-E 2, OpenAI, Darth-D)",
                    prompt={"widget_type": "TextEdit"},
                    image_size={"choices": [256, 512, 1024]})
-def replace_gui(input_image:"napari.types.ImageData", mask:"napari.types.LabelsData", prompt:str = "A similar pattern like in the rest of the image", image_size:int=256, num_images:int = 1) -> "napari.types.ImageData":
+def replace_gui_dalle2(input_image:"napari.types.ImageData", mask:"napari.types.LabelsData", prompt:str = "A similar pattern like in the rest of the image", image_size:int=256, num_images:int = 1) -> "napari.types.ImageData":
     from ._replace import replace
-    image = replace(input_image=input_image, mask=mask, prompt=prompt, image_size=image_size, num_images=num_images)
+    image = replace(input_image=input_image, mask=mask, prompt=prompt, image_width=image_size, image_height=image_size, num_images=num_images, model='dall-e-2')
 
     try:
         from napari.utils.notifications import show_warning
@@ -90,13 +90,54 @@ def replace_gui(input_image:"napari.types.ImageData", mask:"napari.types.LabelsD
     
 
 
+@register_function(menu="Generate > Replace masked region (DALL-E 3, OpenAI, Darth-D)",
+                   prompt={"widget_type": "TextEdit"},
+                   image_size={"choices": ["1024x1024", "1024x1792", "1792x1024"]})
+def replace_gui_dalle3(input_image:"napari.types.ImageData", mask:"napari.types.LabelsData", prompt:str = "A similar pattern like in the rest of the image", image_size:str="1024x1024") -> "napari.types.ImageData":
+    from ._replace import replace
+
+    image_size = image_size.split("x")
+    image_width = int(image_size[0])
+    image_height = int(image_size[1])
+
+    image = replace(input_image=input_image, mask=mask, prompt=prompt, image_width=image_width, image_height=image_height, model='dall-e-3')
+
+    try:
+        from napari.utils.notifications import show_warning
+        show_warning("Using the replace function on scientific images could be seen as scientific misconduct. Handle this function with care.")
+    except:
+        pass
+
+    return image
 
 
 @register_function(menu="Generate > Replace entire image (DALL-E 2, OpenAI, Darth-D)",
+                   prompt={"widget_type": "TextEdit"},
                    image_size={"choices": [256, 512, 1024]})
-def replace_entire_image_gui(input_image:"napari.types.ImageData", prompt:str = "A similar pattern like in the rest of the image", image_size:int=256, num_images:int = 1) -> "napari.types.ImageData":
+def replace_entire_image_gui_dalle2(input_image:"napari.types.ImageData", prompt:str = "A similar pattern like in the rest of the image", image_size:int=256, num_images:int = 1) -> "napari.types.ImageData":
     from ._replace import replace
     image = replace(input_image=input_image, mask=None, prompt=prompt, image_size=image_size, num_images=num_images)
+
+    try:
+        from napari.utils.notifications import show_warning
+        show_warning("Using the replace function on scientific images could be seen as scientific misconduct. Handle this function with care.")
+    except:
+        pass
+
+    return image
+
+
+@register_function(menu="Generate > Replace entire image (DALL-E 3, OpenAI, Darth-D)",
+                   prompt={"widget_type": "TextEdit"},
+                   image_size={"choices": ["1024x1024", "1024x1792", "1792x1024"]})
+def replace_entire_image_gui_dalle2(input_image:"napari.types.ImageData", prompt:str = "A similar pattern like in the rest of the image", image_size:str="1024x1024") -> "napari.types.ImageData":
+    from ._replace import replace
+
+    image_size = image_size.split("x")
+    image_width = int(image_size[0])
+    image_height = int(image_size[1])
+
+    image = replace(input_image=input_image, mask=None, prompt=prompt, image_width=image_width, image_height=image_height, model='dall-e-3')
 
     try:
         from napari.utils.notifications import show_warning
--- darth_d/_replace.py
+++ darth_d/_replace.py
@@ -2,9 +2,9 @@ from stackview import jupyter_displayable_output
 from warnings import warn
 
 @jupyter_displayable_output(library_name='darth-d', help_url='https://github.com/haesleinhuepf/darth-d')
-def replace(input_image, mask = None, prompt:str = "A similar pattern like in the rest of the image", image_size:int=256, num_images:int = 1):
+def replace(input_image, mask = None, prompt:str = "A similar pattern like in the rest of the image", image_width:int=1024, image_height:int=1024, num_images:int = 1, model:str="dall-e-3"):
     """
-    Replace a masked region in an image with a new pattern as described in a prompt using OpenAI's DALL-E 2.
+    Replace a masked region in an image with a new pattern as described in a prompt using OpenAI's DALL-E 2 or 3.
     In case no mask is given, the entire image is replaced in a two-step process:
     First the upper left and lower bottom half of the image is replaced. Afterwards the other two quadrants.
     This may lead to artifacts at quadrant borders.
@@ -14,8 +14,14 @@ def replace(input_image, mask = None, prompt:str = "A similar pattern like in th
     input_image: 2D image, potentially RGB
     mask: 2D image, optional
     prompt: str, optional
-    image_size: int, optional (only 256, 512, 1024 allowed)
     num_images: int, optional
+        ignored for dall-e-3
+    model: str, optional
+        "dall-e-2", "dall-e-3"
+    image_width: int, optional
+        must be 256, 512 or 1024 for dall-e-2 or 1024, 1792 for dall-e-3
+    image_height: int, optional
+        must be 256, 512 or 1024 for dall-e-2 or 1024, 1792 for dall-e-3
 
     See Also
     --------
@@ -42,26 +48,26 @@ def replace(input_image, mask = None, prompt:str = "A similar pattern like in th
         mask[int(mask.shape[0] / 2):, int(mask.shape[1] / 2):] = 1
 
         # replace two quadrants of the image
-        half_replaced = replace(input_image=input_image, mask=mask, prompt=prompt, image_size=image_size, num_images=num_images)
+        half_replaced = replace(input_image=input_image, mask=mask, prompt=prompt, image_width=image_width, image_height=image_height, num_images=num_images, model=model)
         mask_inverse = ((mask == 0) * 1).astype(dtype=np.uint8)
 
         # replace the other two quadrants
         if num_images > 1:
             # in case multiple images were requested, we need to process the individual half-replaced images
             replaced = np.asarray([
-                replace(input_image=half_replaced_image, mask=mask_inverse, prompt=prompt, image_size=image_size,
-                        num_images=1) for half_replaced_image in half_replaced])
+                replace(input_image=half_replaced_image, mask=mask_inverse, prompt=prompt, image_width=image_width,
+                        image_height=image_height, num_images=1, model=model) for half_replaced_image in half_replaced])
         else:
-            replaced = replace(input_image=half_replaced, mask=mask_inverse, prompt=prompt, image_size=image_size,
-                        num_images=num_images)
+            replaced = replace(input_image=half_replaced, mask=mask_inverse, prompt=prompt, image_width=image_width,
+                        image_height=image_height, num_images=num_images, model=model)
 
         return replaced
 
     # in case mask is given
 
     # we rescale image and mask to the specified size
-    resized_image = transform.resize(input_image, (image_size, image_size), anti_aliasing=True)
-    resized_mask = transform.resize(mask, (image_size, image_size), anti_aliasing=False)
+    resized_image = transform.resize(input_image, (image_height, image_width), anti_aliasing=True)
+    resized_mask = transform.resize(mask, (image_height, image_width), anti_aliasing=False)
 
     resized_image_rgb = _img_to_rgb(resized_image)
     resized_image_rgb = (resized_image_rgb * 255 / resized_image_rgb.max()).astype(np.uint8)
@@ -73,7 +79,7 @@ def replace(input_image, mask = None, prompt:str = "A similar pattern like in th
         resized_image_rgb[:, :, 2],
         (resized_mask == 0) * 255]), 0, 2), 0, 1)).astype(np.uint8)
 
-    # actual request to OpenAI's DALL-E 2
+    # actual request to OpenAI's DALL-E 2/3
     client = OpenAI()
 
     response = client.images.edit(
@@ -81,7 +87,7 @@ def replace(input_image, mask = None, prompt:str = "A similar pattern like in th
       mask=numpy_to_bytestream(masked),
       prompt=prompt,
       n=num_images,
-      size=f"{image_size}x{image_size}"
+      size=f"{image_width}x{image_height}",
     )
 
     # bring result in right format
